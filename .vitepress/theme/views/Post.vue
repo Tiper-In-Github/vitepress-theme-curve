593595a7ec94c513e00ac0a02246c750776618a4
无别 <62596032+Tiper-In-Github@users.noreply.github.com>
2024-11-02 17:03:57 +0800

    fix:优化过期提醒并更正错别字

--- .vitepress/theme/views/Post.vue
+++ .vitepress/theme/views/Post.vue
@@ -54,7 +54,7 @@
       <article class="post-article s-card">
         <!-- 过期提醒 -->
         <div class="expired s-card" v-if="postMetaData?.expired >= 180">
-          本文发表于 <strong>{{ postMetaData?.expired }}</strong> 天前，其中的信息可能已经事过境迁
+          本文发表于 <strong>{{ postMetaData?.expired }}</strong> 天前，时过境迁，其中的信息可能已经不再适用性
         </div>
         <!-- AI 摘要 -->
         <ArticleGPT />
